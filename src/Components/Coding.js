b5c5b2b7ef6f55f0a1e25d2ee65756b49f2682d7
prayag <prayagd21@gmail.com>
2020-02-13 12:02:37 +0530

    coding tools added

--- src/Components/Coding.js
+++ src/Components/Coding.js
@@ -1,5 +1,8 @@
 import React from 'react';
+
 const resources = [["Freecodecamp", "https://www.freecodecamp.org/ "],["The Odin Project", "https://www.theodinproject.com/home"], ["Scrimba", "https://scrimba.com/"], ["Flex-cheatsheet", "https://yoksel.github.io/flex-cheatsheet/"], ["Flexbox Froggy", "https://flexboxfroggy.com/"], ["Grid Garden", "https://cssgridgarden.com/"], ["Javascript Documentation", "https://javascript.info/"], ["Watch and Code", "https://watchandcode.com/courses/enrolled/60264"]]
+const tools = [["Codepen", "https://codepen.io/"], ["Github", "https://github.com/"], ["Stackoverflow", "https://stackoverflow.com/"], ["ReplIt", "https://repl.it/"], ["Glitch", "https://glitch.com/"], ["VS Code", "https://code.visualstudio.com/"], ["CodeByte", "https://coderbyte.com/"], ["Codewars", "https://www.codewars.com/"]];
+
 
 const Coding = () => {
     return (
@@ -8,11 +11,16 @@ const Coding = () => {
             <ol>
                 <li>Useful Links</li>
                 <ul>
-                    {resources.map((val) => <a href = {val[1]} target = "_blank"><li>{val[0]}</li></a>)}
+                    {resources.map((val) => <a href = {val[1]} target = "_blank" rel="noopener noreferrer"><li>{val[0]}</li></a>)}
                 </ul>
                 <li>Tools</li>
-
+                <ul>
+                    {tools.map((val) => <a href = {val[1]} target = "_blank" rel="noopener noreferrer"><li>{val[0]}</li></a>)}
+                </ul>
                 <li>People to follow on Twitter</li>
+                <ul>
+                    
+                </ul>
             </ol>
             
         </div>
