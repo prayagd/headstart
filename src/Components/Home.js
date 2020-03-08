ee43349134e4557bcc0736fc93e916cd6959906d
prayag <prayagd21@gmail.com>
2020-03-08 18:13:36 +0530

    fixed the description

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,9 +7,9 @@ const Home = () => {
         <div className = "home">
             <img src = {Headstart} alt = "getting headstart"/>
             <h2>Start upskilling now</h2>
-            <p>This website is a directory of all the tools/articles that i have found useful. While learning  to code and get more interested into writing and social media marketing. Most of the links here are free. If you have know any such free resources to learn which have high signal to noise ratio. Please share them with me on <a href = "https://twitter.com/meprayag" id = "twitter" target = "_blank" rel="noopener noreferrer">twitter</a>. So that i could add them here.</p>
+            <p>While learning to code and exploring about writing and social media marketing. I have come across some useful links. In order to share those links with everyone and to test my coding skills. I have created this directory. Most of the links are free. If you know any more free resources to learn. Please share them with me on <a href = "https://twitter.com/meprayag" id = "twitter" target = "_blank" rel="noopener noreferrer">twitter</a>.</p>
         </div>
     )
 }
 
-export default  Home
+export default Home
